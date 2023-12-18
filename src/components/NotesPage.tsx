ca50eb9945f32dd49a9c94ac9742bf807e7059ec
eben4ya <benaya.imanuela@gmail.com>
2023-12-18 23:53:16 +0700

    <feat: add fix design>

--- src/components/NotesPage.tsx
+++ src/components/NotesPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const NotesPage: React.FC = () => {
   return (
     <>
-      <section className="flex flex-col justify-center items-center w-full h-screen">
+      {/* <section className="flex flex-col justify-center items-center w-full h-screen">
         <label
           htmlFor="firstFolder"
           className="text-[#242424] text-[76.8px] font-normal leading-normal"
@@ -16,49 +17,40 @@ const NotesPage: React.FC = () => {
           placeholder="+  Create your first folder here"
           className="w-[59.375%] h-fit mt-[28px] p-[10px] border-solid border-[1px] border-[#BEBEBE] rounded-[8px] "
         />
-      </section>
-      {/* <section className="absolute top-0 right-0 flex flex-col w-[66.153%] h-screen ">
-        <div className="w-fit h-fit ml-[44px] font-normal leading-normal">
-          <p className="w-fit h-fit mt-[53px] text-[17.067px]">College / new</p>
-          <h1 className="w-fit h-fit mt-[29px] text-[42.67px] font-bold">
-            Title
-          </h1>
-          <p className="w-fit h-fit mt-[12px] text-[19.2px]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor
-            ligula nec pretium faucibus. Phasellus accumsan a augue et
-            efficitur.
-          </p>
-          <p className="w-fit h-fit mt-[32px] text-[19.2px]">
-            Aenean maximus ullamcorper est, nec pretium dui dapibus ut. Nullam
-            arcu tortor, dignissim id orci ac, vestibulum posuere ipsum.
-            Suspendisse vel augue eget libero scelerisque euismod. Suspendisse
-            vulputate erat id est ultrices, vel ultrices ligula ornare. Aliquam
-            libero lacus, egestas eu arcu in, sodales pharetra ipsum. Nulla
-            semper metus vel porttitor ornare. Praesent ultricies, justo ac
-            volutpat rhoncus, nulla urna consectetur massa, a porttitor felis
-            sapien at nibh. Aenean maximus ullamcorper est, nec pretium dui
-            dapibus ut. Nullam arcu tortor, dignissim id orci ac, vestibulum
-            posuere ipsum. Suspendisse vel augue eget libero scelerisque
-            euismod. Suspendisse vulputate erat id est ultrices, vel ultrices
-            ligula ornare. Aliquam libero lacus, egestas eu arcu in, sodales
-            pharetra ipsum. Nulla semper metus vel porttitor ornare. Praesent
-            ultricies, justo ac volutpat rhoncus, nulla urna consectetur massa,
-            a porttitor felis sapien at nibh. Aenean maximus ullamcorper est,
-            nec pretium dui dapibus ut. Nullam arcu tortor, dignissim id orci
-            ac, vestibulum posuere ipsum. Suspendisse vel augue eget libero
-            scelerisque euismod. Suspendisse vulputate erat id est ultrices, vel
-            ultrices ligula ornare. Aliquam libero lacus, egestas eu arcu in,
-            sodales pharetra ipsum. Nulla semper metus vel porttitor ornare.
-            Praesent ultricies, justo ac volutpat rhoncus, nulla urna
-            consectetur massa, a porttitor felis sapien at nibh. Aenean maximus
-            ullamcorper est, nec pretium dui dapibus ut. Nullam arcu tortor,
-            dignissim id orci ac, vestibulum posuere ipsum. Suspendisse vel
-            augue eget libero scelerisque euismod. Suspendisse vulputate erat id
-            est ultrices, vel ultrices ligula ornare. Aliquam libero lacus,
-            egestas eu arcu in, sodales{" "}
-          </p>
-        </div>
       </section> */}
+      <section className="absolute top-0 right-0 flex flex-col justify-center items-center w-[70%] h-screen border-l-2">
+        <form className="flex flex-col w-[95%] h-full font-normal leading-normal">
+          <p className="w-fit h-fit mt-[53px] text-[17.067px]">College / new</p>
+          <input
+            className="mt-[29px] text-[42.67px] font-bold tracking-wider"
+            type="text"
+            placeholder="Title"
+          />
+          <textarea
+            rows={3}
+            className="mt-[12px] text-[19.2px] tracking-wide"
+            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor ligula nec pretium faucibus. Phasellus accumsan a augue et efficitur."
+          />
+          <textarea
+            rows={25}
+            className="mt-[32px] text-[19.2px]"
+            placeholder="Aenean maximus ullamcorper est, nec pretium dui dapibus ut. Nullam arcu tortor, dignissim id orci ac, vestibulum posuere ipsum. Suspendisse vel augue eget libero scelerisque euismod. Suspendisse vulputate erat id est ultrices, vel ultrices ligula ornare. Aliquam libero lacus, egestas eu arcu in, sodales pharetra ipsum. Nulla semper metus vel porttitor ornare. Praesent ultricies, justo ac volutpat rhoncus, nulla urna consectetur massa, a porttitor felis sapien at nibh.
+                         Aenean maximus ullamcorper est, nec pretium dui dapibus ut. Nullam arcu tortor, dignissim id orci ac, vestibulum posuere ipsum. Suspendisse vel augue eget libero scelerisque euismod. Suspendisse vulputate erat id est ultrices, vel ultrices ligula ornare. Aliquam libero lacus, egestas eu arcu in, sodales pharetra ipsum. Nulla semper metus vel porttitor ornare. Praesent ultricies, justo ac volutpat rhoncus, nulla urna consectetur massa, a porttitor felis sapien at nibh.
+                         Aenean maximus ullamcorper est, nec pretium dui dapibus ut. Nullam arcu tortor, dignissim id orci ac, vestibulum posuere ipsum. Suspendisse vel augue eget libero scelerisque euismod. Suspendisse vulputate erat id est ultrices, vel ultrices ligula ornare. Aliquam libero lacus, egestas eu arcu in, sodales pharetra ipsum. Nulla semper metus vel porttitor ornare. Praesent ultricies, justo ac volutpat rhoncus, nulla urna consectetur massa, a porttitor felis sapien at nibh.
+                         Aenean maximus ullamcorper est, nec pretium dui dapibus ut. Nullam arcu tortor, dignissim id orci ac, vestibulum posuere ipsum. Suspendisse vel augue eget libero scelerisque euismod. Suspendisse vulputate erat id est ultrices, vel ultrices ligula ornare. Aliquam libero lacus, egestas eu arcu in, sodales pharetra ipsum. Nulla semper metus vel porttitor ornare. Praesent ultricies, justo ac volutpat rhoncus, nulla urna consectetur massa, a porttitor felis sapien at nibh.
+                         Aenean maximus ullamcorper est, nec pretium dui dapibus ut. Nullam arcu tortor, dignissim id orci ac, vestibulum posuere ipsum. Suspendisse vel augue eget libero scelerisque euismod. Suspendisse vulputate erat id est ultrices, vel ultrices ligula ornare. Aliquam libero lacus, egestas eu arcu in, sodales pharetra ipsum. Nulla semper metus vel porttitor ornare. Praesent ultricies, justo ac volutpat rhoncus, nulla urna consectetur massa, a porttitor felis sapien at nibh."
+          />
+          <button
+            className="absolute z-10 bottom-[10%] right-[10%] w-[10%] h-fit p-[10px] bg-[#F8F8F8] border-[1px] border-solid border-[#BEBEBE] rounded-[8px] drop-shadow-xl "
+            type="submit"
+          >
+            <div className="flex justify-center items-center gap-[10px] ">
+              <FaArrowDownLong className="w-[20px] h-[20px]" />
+              <p>Save</p>
+            </div>
+          </button>
+        </form>
+      </section>
     </>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,72 @@
 import React from "react";
+import { CiCirclePlus } from "react-icons/ci";
+import { IoIosArrowUp } from "react-icons/io";
+import { FaTrash } from "react-icons/fa6";
 
 const Sidebar: React.FC = () => {
   return (
-    <aside className="fixed top-0 left-0 w-[33.847%] h-screen ">
-      <h2 className="w-fit h-fit mt-[62px] ml-[42px] text-[#242424] text-[34.13px] font-bold leading-normal">
-        Notes
-      </h2>
+    <aside className="fixed top-0 left-0 flex flex-col items-center w-[30%] h-screen ">
+      <div className="w-[75.2%] h-fit mt-[62px] ">
+        <h2 className="w-fit h-fit text-[#242424] text-[34.13px] font-bold leading-normal">
+          Notes
+        </h2>
+        <div className="flex flex-col mt-[14px] ">
+          <div className="flex flex-row justify-between items-center">
+            <h6 className="text-[17.067px] text-[#242424] font-medium leading-normal tracking-wider opacity-50">
+              Folders
+            </h6>
+            <div className="flex w-fit h-fit gap-[10px]">
+              <CiCirclePlus className="w-[25px] h-[25px]" />
+              <IoIosArrowUp className="w-[25px] h-[25px]" />
+            </div>
+          </div>
+          <div className="flex flex-col gap-[5px] mt-4">
+            <p className="p-[10px] hover:bg-[#F8F8F8] hover:rounded-[8px] text-[19.2px] text-[#242424] font-normal hover:font-medium leading-normal tracking-wide">
+              College
+            </p>
+            <p className="p-[10px] hover:bg-[#F8F8F8] hover:rounded-[8px] text-[19.2px] text-[#242424] font-normal hover:font-medium leading-normal tracking-wide">
+              Work
+            </p>
+            <p className="p-[10px] hover:bg-[#F8F8F8] hover:rounded-[8px] text-[19.2px] text-[#242424] font-normal hover:font-medium leading-normal tracking-wide">
+              Home
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col mt-[32px] ">
+          <div className="flex flex-row justify-between items-center">
+            <h6 className="text-[17.067px] text-[#242424] font-medium leading-normal tracking-wider opacity-50">
+              College
+            </h6>
+            <IoIosArrowUp className="w-[25px] h-[25px]" />
+          </div>
+          <div className="flex flex-col gap-[5px] mt-4">
+            <div className="flex justify-between items-center hover:bg-[#F8F8F8] hover:rounded-[8px] p-[10px]">
+              <p className="text-[19.2px] text-[#242424] font-normal leading-normal tracking-wide">
+                Calculus
+              </p>
+              <FaTrash className="w-[14px] h-[18px] opacity-50" />
+            </div>
+            <div className="flex justify-between items-center hover:bg-[#F8F8F8] hover:rounded-[8px] p-[10px]">
+              <p className="text-[19.2px] text-[#242424] font-normal leading-normal tracking-wide">
+                Physics
+              </p>
+              <FaTrash className="w-[14px] h-[18px] opacity-50" />
+            </div>
+            <div className="flex justify-between items-center hover:bg-[#F8F8F8] hover:rounded-[8px] p-[10px]">
+              <p className="text-[19.2px] text-[#242424] font-normal leading-normal tracking-wide">
+                Math
+              </p>
+              <FaTrash className="w-[14px] h-[18px] opacity-50" />
+            </div>
+            <div className="flex justify-between items-center hover:bg-[#F8F8F8] hover:rounded-[8px] p-[10px]">
+              <p className="text-[19.2px] text-[#242424] font-normal leading-normal tracking-wide">
+                Chem
+              </p>
+              <FaTrash className="w-[14px] h-[18px] opacity-50" />
+            </div>
+          </div>
+        </div>
+      </div>
     </aside>
   );
 };
